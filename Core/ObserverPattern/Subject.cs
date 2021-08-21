1f97eab0f5381b900d8a5cfc5780a0610dffdf8b
Jeffry Miranda Toruno <jeffryuna@gmail.com>
2021-08-21 14:06:33 -0600

    Observer Pattern

---
+++ Core/ObserverPattern/Subject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Core.ObserverPattern
+{
+    public class Subject
+    {
+        private readonly List<IObserver> observerList;
+        public Subject()
+        {
+            observerList = new List<IObserver>();
+        }
+
+        public void Register(IObserver observer)
+        {
+            observerList.Add(observer);
+        }
+
+        protected void Notify()
+        {
+            foreach (IObserver observer in observerList)
+            {
+                observer.Update((Sensor)this);
+            }
+        }
+    }
+}
---
+++ Test/ObserverPattern/ObserverPatternTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Core.ObserverPattern;
+using NUnit.Framework;
+
+namespace Test.ObserverPattern
+{
+    public class ObserverPatternTest
+    {
+        [Test]
+        public void TestDynamicCollaboration()
+        {
+            Console.WriteLine("===");
+            Console.WriteLine($"Observer Pattern!");
+            Console.WriteLine("===");
+            // Creating the metters 
+            Meter meter1 = new("meter#1");
+            Meter meter2 = new("meter#2");
+            // Register the meter with the Subject base class of the Sensor.
+            Sensor sensor = new(5);
+            sensor.Register(meter1);
+            sensor.Register(meter2);
+
+            // Starting collaboration
+            sensor.Check();
+            Console.WriteLine("===");
+            Assert.Pass();
+        }
+    }
+}
